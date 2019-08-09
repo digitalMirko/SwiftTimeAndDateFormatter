5c5dc569ee455266bcc969f8397da9117da6a622
Mirko Cukich <mirkocukich@gmail.com>
2019-08-08 22:38:10 -0500

    Initial Commit

---
+++ Time and Date Formatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Time and Date Formatter
+//
+//  Created by Mirko Cukich on 8/8/19.
+//  Copyright © 2019 Digital Mirko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
